4e81505e3be6b252436fbe853e8a24074552096e
James Strong <itsjms@me.com>
2019-09-30 17:12:57 -0400

    Setup routing. Add API endpoint to create invoice. Delay due to Emirs printing issues

--- app/controllers/invoice_controller.rb
+++ app/controllers/invoice_controller.rb
@@ -1,4 +1,21 @@
 class InvoiceController < ApplicationController
   def index
+    
   end
+  
+  def create
+    Invoice.create(
+      invoice_number: params[:invoice_number],
+      total: params[:total],
+      currency: params[:currency],
+      invoice_date: params[:invoice_date],
+      due_date: params[:due_date],
+      vendor_name: params[:vendor_name],
+      remittance_address: params[:remittance_address]
+    )
+    
+    render status: 200, json: {
+      message: "invoice submitted successfully"
+    }
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'invoice/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'Invoice', to: "invoice#create"
+  root "invoice#index"
 end
