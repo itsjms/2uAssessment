97c1f6a327a597e1e69e4efa60e6c38c0af9fa55
James Strong <itsjms@me.com>
2019-09-30 17:18:09 -0400

    Add annotate so we can see what we've got in models

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: invoices
+#
+#  id                 :integer          not null, primary key
+#  invoice_number     :string
+#  total              :float
+#  currency           :string
+#  invoice_date       :datetime
+#  due_date           :datetime
+#  vendor_name        :string
+#  remittance_address :string
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
+
 class Invoice < ApplicationRecord
 end
--- test/models/invoice_test.rb
+++ test/models/invoice_test.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: invoices
+#
+#  id                 :integer          not null, primary key
+#  invoice_number     :string
+#  total              :float
+#  currency           :string
+#  invoice_date       :datetime
+#  due_date           :datetime
+#  vendor_name        :string
+#  remittance_address :string
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
+
 require 'test_helper'
 
 class InvoiceTest < ActiveSupport::TestCase
